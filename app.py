28593183e801660164da4204a8ba3f62b54eebf9
intarios <intars.matisans@edu.rtu.lv>
2024-05-02 14:29:37 +0300

    Revert "Fix app"

--- app.py
+++ app.py
@@ -7,4 +7,9 @@ def greetings():
     return jsonify({"greeting": "Hello from Python App!"})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port="3000")
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', default=5000, type=int, help='Port to run the server on.')
+    args = parser.parse_args()
+    
+    app.run(debug=True, port=args.port)
